db46c476c4f983716e7262abf6652db8ba84d9da
Devin Dow <DevinDow@gmail.com>
2023-10-28 04:36:24 -0700

    added Animation & AnimationController

--- lib/board.dart
+++ lib/board.dart
@@ -5,6 +5,7 @@ import 'package:othello/situation.dart';
 import 'package:othello/board_painter.dart';
 import 'package:othello/alert.dart';
 
+import 'dart:math' as math;
 import 'dart:developer' as dev;
 
 class Board extends StatefulWidget {
@@ -19,7 +20,43 @@ class Board extends StatefulWidget {
   State<Board> createState() => _BoardState();
 }
 
-class _BoardState extends State<Board> {
+class _BoardState extends State<Board> with TickerProviderStateMixin {
+  late Animation<double> animation;
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+
+    Tween<double> rotationTween = Tween(begin: -math.pi, end: math.pi);
+
+    animation = rotationTween.animate(controller)
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          //controller.repeat();
+        } else if (status == AnimationStatus.dismissed) {
+          controller.forward();
+        }
+      });
+
+    // start the Animation
+    controller.forward();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     if (Board.logBoardBuildSituation) {
@@ -34,14 +71,21 @@ class _BoardState extends State<Board> {
               .maxHeight, // make it a square based on Expanded's Height
           child: GestureDetector(
             onTapDown: (details) => _onTapDown(details, context),
-            child:
-                CustomPaint(painter: BoardPainter(situation: widget.situation)),
+            child: AnimatedBuilder(
+              animation: animation,
+              builder: (context, snapshot) {
+                return CustomPaint(
+                    painter: BoardPainter(situation: widget.situation));
+              },
+            ),
           ),
         ),
       ),
     );
   }
 
+  /// User clikced the Board
+  /// figure out which Coord and call Game's makeMoveCallback()
   _onTapDown(TapDownDetails details, BuildContext context) {
     if (widget.situation.endOfGame) {
       Alert(context, "Game Over", "press New Game to play again");
